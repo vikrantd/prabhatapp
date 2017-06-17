04ab39aa1282bcd647cb910340595f2b49603b00
Prabhat Dubey <prabhatdubey2002@gmail.com>
2017-06-17 21:20:08 +0530

    Update

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-alert('hellp from script.js');
+alert('Welcome to geocode');
+
+function GetLocation() {
+            var geocoder = new google.maps.Geocoder();
+            var address = document.getElementById("txtAddress").value;
+            geocoder.geocode({ 'address': address }, function (results, status) {
+                if (status == google.maps.GeocoderStatus.OK) {
+                    var latitude = results[0].address_components.administrative_area_level_1.long_name();
+                } else {
+                    alert("Request failed.")
+                }
+            });
+        };
