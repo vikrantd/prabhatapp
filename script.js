1942668f3cbcabc391a0274f874c92122323f2bd
QUAERO\dubeyv <vikrantdubey94@gmail.com>
2017-07-13 23:31:27 +0530

    Tango charlie

--- script.js
+++ script.js
@@ -2,23 +2,17 @@ alert('Welcome to geocode');
 var input = document.getElementById("textbox");
 
 
-function GetLocation() {
+function pin() {
             var geocoder = new google.maps.Geocoder();
             var address = document.getElementById("textbox").value;
             geocoder.geocode({ 'address': address }, function (results, status) {
                 if (status == google.maps.GeocoderStatus.OK) {
-                    var latitude = results[0].address_components.administrative_area_level_1.long_name();
+                    console.log(results);
+                    var latitude = results[0].formatted_address;
+                    alert(latitude);
                 } else {
                     alert("Request failed.")
                 }
             });
         };
 
-
-
-   /* var input = document.getElementById('textbox');   
-        function pin()
-        {
-        alert(textbox.value);
-        }
-        */
